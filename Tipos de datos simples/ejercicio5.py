057e592034ce0b8de3052be5dc9bd39416b13db2
Oscarsp15 <oscar.sirlopu@gmail.com>
2024-06-11 14:21:06 -0500

    6to  ejercicio

--- Tipos de datos simples/ejercicio5.py
+++ Tipos de datos simples/ejercicio5.py
@@ -1,7 +1,7 @@
 #Escribir un programa que pregunte al usuario por el número de 
 #horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.
 
-Htrabajadas = int(input("¿cuantos horas tienes trabajadas?: \n"))
-CxHora= int(input("¿cuanto es el coste por hora?: \n"))
+Htrabajadas = float(input("¿cuantos horas tienes trabajadas?: \n"))
+CxHora= float(input("¿cuanto es el coste por hora?: \n"))
 pago = Htrabajadas * CxHora
 print (f"Tu pago es: S./{pago}") #Utilizo la moneda de mi peru el sol peruano "PEN"
---
+++ Tipos de datos simples/ejercicio6.py
@@ -0,0 +1,12 @@
+#Escribir un programa que lea un entero positivo, 
+# introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta. 
+# La suma de los primeros enteros positivos puede ser calculada de la siguiente forma:
+# suma = (n(n+1))/2
+
+n = int(input("Introduce un entero positivo: \n"))
+suma = (n * (n +1))/2
+print(f"La suma de los numeros enteros comenzando de 1 hasta {n} es: {suma}")
+
+
+
+              
